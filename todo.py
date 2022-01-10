933100bd0ebc50a1a3851962aef97ccd34dff8e5
richardli03 <rli@olin.edu>
2022-01-10 17:56:28 -0500

    Created slightly functional todo function
    todo lists todos, todoAdd can currently add todos to the list of todos if you type it in double quotes

--- todo.py
+++ todo.py
@@ -1,5 +1,12 @@
 #accessing the calendar
 
-
+import csv
 def todo():
-    return True
+    with open("todo.csv", "r") as file:
+        todoList = list(csv.reader(file))
+    return todoList
+
+if __name__ == "__main__":
+    print("This is your current todo list: ")
+    print(*todo(), sep = "\n")
+
---
+++ todoAdd.py
@@ -0,0 +1,33 @@
+import sys
+import csv
+
+# total arguments
+
+def add(newTodoString):
+    with open("todo.csv", "a") as file:
+        writer = csv.writer(file)
+        
+        #NOTE: you have to put "newTodoString" in brackets because otherwise the writer treats it like a sequence of characters.
+        writer.writerow([newTodoString])
+    return True
+
+if __name__ == "__main__":
+    try: 
+        sys.argv[1]
+    except IndexError:
+        print("Please type something to add to your to-do list!")
+        exit()  
+
+    command = '';  
+    for arg in sys.argv[1:]:          # skip sys.argv[0] since the question didn't ask for it
+        if ' ' in arg:
+            command+= '"{}" '.format(arg) ;   # Put the quotes back in
+        else:
+            command+="{} ".format(arg) ;      # Assume no space => no quotes
+
+    newTodoString = str(command.replace('"', ""))
+
+    if add(newTodoString) == True:
+        print(str(sys.argv[1:]) + " has been added to your todo list")
+    else: 
+        print("Sorry! There was an error adding this item to your todo")
