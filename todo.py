7cb9192b6aa449b864d648ab317252a7014923f2
richardli03 <rli@olin.edu>
2022-01-10 21:32:39 -0500

    todoAdd function works!
    Using a dictionary, now able to add an index number and a name to each task. This will make it easier to remove tasks by both number and name.

--- todo.py
+++ todo.py
@@ -3,10 +3,17 @@
 import csv
 def todo():
     with open("todo.csv", "r") as file:
-        todoList = list(csv.reader(file))
+        csv_reader = (csv.reader(file))
+        next(csv_reader)
+        todoList = list(csv_reader)
+        
     return todoList
 
 if __name__ == "__main__":
     print("This is your current todo list: ")
-    print(*todo(), sep = "\n")
+
+    for task in todo():
+        print (task[0],task[1])
+
+   #print(*todo(), sep = "\n")
 
--- todoAdd.py
+++ todoAdd.py
@@ -1,15 +1,27 @@
 import sys
 import csv
+import pandas 
 
-# total arguments
 
 def add(newTodoString):
-    with open("todo.csv", "a") as file:
-        writer = csv.writer(file)
 
-        #NOTE: you have to put "newTodoString" in brackets because otherwise the writer treats it like a sequence of characters.
-        writer.writerow([newTodoString])
+    
+    with open("todo.csv", "r") as f:
+        reader = csv.reader(f)
+        length = len(list(reader))
+        print(length)
+    f.close()
+    
+    headers = ['index','task']  
+    newTask={'index': (length) ,'task': (newTodoString)}
+    
+    with open("todo.csv", "a", newline ='') as f:
+        dictwriter = csv.DictWriter(f, fieldnames=headers)
+        dictwriter.writerow(newTask)
+    f.close()
+        
     return True
+
 
 if __name__ == "__main__":
     try: 
@@ -19,7 +31,7 @@ if __name__ == "__main__":
         exit()  
 
     command = '';  
-    for arg in sys.argv[2:]:          # skip sys.argv[0] since the question didn't ask for it
+    for arg in sys.argv[2:]:          
         if ' ' in arg:
             command+= '"{}" '.format(arg) ;   # Put the quotes back in
         else:
@@ -28,6 +40,6 @@ if __name__ == "__main__":
     newTodoString = str(command.replace('"', ""))
 
     if add(newTodoString) == True:
-        print(str(sys.argv[2:]) + " has been added to your todo list")
+        print(str([newTodoString]) + " has been added to your todo list")
     else: 
         print("Sorry! There was an error adding this item to your todo")
