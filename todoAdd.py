43e9a2c31f31e815d6803530ce3148dfa7050c02
richardli03 <rli@olin.edu>
2022-01-11 21:18:57 -0500

    TodoADD now quits if you input a number
    The "task-finishing" file makes it so you can't input a number as the task or it'll break the indexing system... I think? Still trying to test, but as far as I can tell, it does.

--- todoAdd.py
+++ todoAdd.py
@@ -2,7 +2,7 @@ import sys
 import csv
 
 
-def add(newTodoString):
+def add(newTodo):
 
     
     with open("todo.csv", "r") as f:
@@ -12,7 +12,7 @@ def add(newTodoString):
     f.close()
     
     headers = ['index','task']  
-    newTask={'index': (length) ,'task': (newTodoString)}
+    newTask={'index': (length) ,'task': (newTodo)}
     
     with open("todo.csv", "a", newline ='') as f:
         dictwriter = csv.DictWriter(f, fieldnames=headers)
@@ -23,22 +23,33 @@ def add(newTodoString):
 
 
 if __name__ == "__main__":
+    
+    # the task will be the 3rd thing entered from the command line (todoAdd.py add ...) 
+    # we check if it exists
     try: 
         sys.argv[2]
     except IndexError:
         print("Please type something to add to your to-do list!")
-        exit()  
+        sys.exit()
+    
+     # we check to make sure task doesn't start with number (confuses todoFin)
 
-    command = '';  
-    for arg in sys.argv[2:]:          
-        if ' ' in arg:
-            command+= '"{}" '.format(arg) ;   # Put the quotes back in
-        else:
-            command+="{} ".format(arg) ;      # Assume no space => no quotes
+    try: 
+        isNumber = isinstance(int(sys.argv[2]),int)
+        print("Please don't start your task with a number")
+        sys.exit()
+    except ValueError:
+    # allows apostrophes if the task is in quotes 
+        command = '';  
+        for arg in sys.argv[2:]:          
+            if ' ' in arg:
+                command+= '"{}" '.format(arg) ;   # Put the quotes back in
+            else:
+                command+="{} ".format(arg) ;      # Assume no space => no quotes
 
-    newTodoString = str(command.replace('"', ""))
+        newTodo = str(command.replace('"', ""))
 
-    if add(newTodoString) == True:
-        print(str([newTodoString]) + " has been added to your todo list")
-    else: 
-        print("Sorry! There was an error adding this task to your todo")
+        if add(newTodo) == True:
+            print(str([newTodo]) + " has been added to your todo list")
+        else: 
+            print("Sorry! There was an error adding this task to your todo")
